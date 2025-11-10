2265f66e11ca88be01aafef293725e8a34ffa99a
qwelord <secondofall22@rambler.ru>
2025-11-10 16:41:38 +0500

    Созданная страница Speciality. Казаков

---
+++ Submission of Applications_Kazakov/Pages/Speciality.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Submission_of_Applications_Kazakov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Speciality.xaml
+    /// </summary>
+    public partial class Speciality : Page
+    {
+        public Speciality()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
